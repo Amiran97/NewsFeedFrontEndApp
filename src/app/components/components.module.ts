d56812e1755677e5ce023c1c4e51e2d300d9c40f
Dmytro Kuz <dmytro.kuz@tangramltd.com>
2022-12-01 16:20:06 +0200

    create posts list and post item components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { HeaderComponent } from "./header/header.component";
 import { ReactiveFormsModule } from "@angular/forms";
+
+import { HeaderComponent } from "./header/header.component";
+import { PostsListComponent } from "./posts-list/posts-list.component";
+import { PostItemComponent } from "./posts-list/post-item/post-item.component";
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, PostsListComponent, PostItemComponent],
+  exports: [HeaderComponent, PostsListComponent, PostItemComponent],
 })
 export class ComponentsModule {}
