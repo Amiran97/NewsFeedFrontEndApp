826357b4bbfb32b1eb0a73786db37d0b34d5a9eb
Dmytro Kuz <dmytro.kuz@tangramltd.com>
2022-12-11 15:23:49 +0200

    merge module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { HeaderComponent } from "./header/header.component";
 import { PostsListComponent } from "./posts-list/posts-list.component";
