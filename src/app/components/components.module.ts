03e817678c0d63851b34c68facf7fe914e1af151
Dmytro Kuz <dmytro.kuz@tangramltd.com>
2022-12-11 15:40:28 +0200

    Merge branch 'dev-dmitry' into dev-dmitry-4

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,10 +6,21 @@ import { RouterModule } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { SideBarComponent } from "./side-bar/side-bar.component";
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
-  declarations: [HeaderComponent, LoginComponent, RegisterComponent],
-  exports: [HeaderComponent, LoginComponent, RegisterComponent],
+  declarations: [
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    SideBarComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    SideBarComponent,
+  ],
 })
 export class ComponentsModule {}
