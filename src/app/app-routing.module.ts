c1d9afaa7f41aca76d6ea26f0f55c121318f537d
Amiran97 <amiran.todua@tangramltd.com>
2022-12-13 18:05:45 +0200

    Minor fix routing module and header pug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
+import { PostsListComponent } from "./components/posts-list/posts-list.component";
 import { RegisterComponent } from "./components/register/register.component";
 
 const routes: Routes = [
+  {
+    path: "",
+    component: PostsListComponent
+  },
   {
     path: "signin",
     component: LoginComponent,
